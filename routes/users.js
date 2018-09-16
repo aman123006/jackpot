5d672cc2ea3eab0c0187cc4a090019343851d751
Amandeep <aman.01298@gmail.com>
2018-09-17 01:40:54 +0530

    Reset Pass

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,46 @@ let User = require('../models/user');
 
 
 
+
+router.post('/changepassword', function(req, res){
+  
+   let user1={password :req.body.newpasword};
+let {password, username}=req.body;
+console.log(password);
+User.findOne({username:username},(error,user)=>
+{
+  
+  if(error)
+    {console.log(error);
+    res.send({error});}
+    else{console.log("Aman");
+    if(!user){
+      res.send({error:"No Such User"});
+      return;
+    }
+      if(user.password === password)
+        { 
+          User.updateOne({username},user1,(error)=>
+    {
+      if(error) 
+        {
+          console.log(err);
+    res.send({error});
+        }else{
+          res.send({success:true});
+        }
+    });
+  }
+  else
+    {
+       res.send({error:"Incorrect Password"});
+    }
+    }
+})
+
+
+});
+
 router.post('/register', function(req, res){
   const name = req.body.name;
   const email = req.body.email;
